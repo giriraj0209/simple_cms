147d0c1f5cfe695499cb23e671126a4cb304ddc5
giriraj0209 <giriraj.kashyapa@gmail.com>
2016-03-24 21:23:52 -0500

    Added slug property for custom url

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  resources :pages, only: [:show]
   namespace :admin do
   resources :pages
+  end
+
+  Page.where.not("slug", nil).all.each do |page|
+  	get "/#{page.slug}", controller: "pages" , action: "show", id: page.id
   end
 end

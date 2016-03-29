f607fd6073c97c659d82e9f501daa6d9434210f1
giriraj0209 <giriraj.kashyapa@gmail.com>
2016-03-29 13:36:38 -0500

    Added Category module and provided custom views

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -5,15 +5,13 @@ class PagesController < ApplicationController
   # GET /pages
   # GET /pages.json
   def index
-    
     @pages = Page.all
   end
 
   # GET /pages/1
   # GET /pages/1.json
   def show
-    #
-    
+   
   end
 
   # GET /pages/new

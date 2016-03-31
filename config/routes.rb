184c9b6cc4af450d330291c5d4099f06436ea879
giriraj0209 <giriraj.kashyapa@gmail.com>
2016-03-30 21:38:44 -0500

    Added wyswyg editor for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
+  mount Ckeditor::Engine => '/ckeditor'
   resources :categories, only: [:show]
   resources :pages, only: [:show]
+
   namespace :admin do
   resources :pages
   resources :categories
   end
-
+  root 'admin/pages#index'
   Page.where.not("slug", nil).all.each do |page|
   	get "/#{page.slug}", controller: "pages" , action: "show", id: page.id
   end
-  root 'admin/pages#index'
+  
 end
